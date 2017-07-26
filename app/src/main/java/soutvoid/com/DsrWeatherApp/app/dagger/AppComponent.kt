77b7addcc4caaf6146b9566d50904ceaf2a21c4d
nadraliev <nadraliev@gmail.com>
2017-07-26 20:56:51 +0300

    added small widget (for now only for the first location in list)

--- app/src/main/java/soutvoid/com/DsrWeatherApp/app/dagger/AppComponent.kt
+++ app/src/main/java/soutvoid/com/DsrWeatherApp/app/dagger/AppComponent.kt
@@ -14,6 +14,7 @@ import soutvoid.com.DsrWeatherApp.interactor.network.NetworkConnectionChecker
 import soutvoid.com.DsrWeatherApp.interactor.uvi.UviModule
 import soutvoid.com.DsrWeatherApp.interactor.uvi.UviRepository
 import soutvoid.com.DsrWeatherApp.ui.base.activity.ActivityModule
+import soutvoid.com.DsrWeatherApp.ui.widget.SmallWidgetProvider
 
 @PerApplication
 @Component(modules = arrayOf(
